eec2f35da88b9813b9f995f6ff189f3607c2e6fc
Volki-v-boi <152771192+Volki-v-boi@users.noreply.github.com>
2024-04-29 06:57:45 +0200

    hw-9-done

---
+++ src/js/2-form.js
@@ -0,0 +1,35 @@
+const formData = {
+  email: '',
+  message: '',
+};
+
+if (localStorage.getItem('feedback-form-state')) {
+  const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  formData.email = savedData.email;
+  formData.message = savedData.message;
+  document.querySelector('input[name="email"]').value = savedData.email;
+  document.querySelector('textarea[name="message"]').value = savedData.message;
+}
+
+document.querySelector('.feedback-form').addEventListener('input', event => {
+  if (
+    event.target.type === 'email' ||
+    event.target.tagName.toLowerCase() === 'textarea'
+  ) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  }
+});
+
+document.querySelector('.feedback-form').addEventListener('submit', event => {
+  event.preventDefault();
+  if (formData.email && formData.message) {
+    console.log(formData);
+    localStorage.removeItem('feedback-form-state');
+    document.querySelector('input[name="email"]').value = '';
+    document.querySelector('textarea[name="message"]').value = '';
+  } else {
+    alert('Будь ласка заповніть всі поля');
+  }
+});
+//============================================================
---
+++ src/main.js
@@ -0,0 +1,3 @@
+import {} from './js/1-gallery.js';
+
+import {} from './js/2-form.js';
